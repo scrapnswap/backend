289ae474b1f2bbf92bd01de75ffb052dbe270bef
Zach Deibert <zachdeibert@gmail.com>
2017-04-20 08:07:43 -0500

    Made it JSON

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Hello, world!</title>
-	</head>
-	<body>
-		<h1>Hello, <?php echo "world"; ?>!</h1>
-	</body>
-</html>
+{
+	"test": <?php echo "\"successful\""; ?>
+}
