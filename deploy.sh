18ffaaa7ab85892ec26ec7d1ed2d53116213d5a1
Zach Deibert <zachdeibert@gmail.com>
2017-04-20 08:05:43 -0500

    Fixed deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_69473c50fa23_key -iv $encrypted_69473c50fa23_iv -in auth.ftp.enc -out auth.ftp -d
-cat auth.ftp deploy.ftp | ftp
+openssl aes-256-cbc -K $encrypted_69473c50fa23_key -iv $encrypted_69473c50fa23_iv -in auth.curl.enc -out auth.curl -d
+curl -T index.php -K auth.curl ftp://files.000webhost.com/public_html/index.php
